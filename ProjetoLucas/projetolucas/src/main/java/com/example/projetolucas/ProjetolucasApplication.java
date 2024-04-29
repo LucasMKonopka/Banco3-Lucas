81c22fc50b2d1b94baf258c671b0b9b716f93af2
LucasMKonopka <150478119+LucasMKonopka@users.noreply.github.com>
2024-04-29 10:05:56 -0300

    first commit

---
+++ ProjetoLucas/projetolucas/src/main/java/com/example/projetolucas/ProjetolucasApplication.java
@@ -0,0 +1,13 @@
+package com.example.projetolucas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetolucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetolucasApplication.class, args);
+	}
+
+}
