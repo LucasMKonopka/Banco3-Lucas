2767bd3c8aa62bfd5e2ad5f524a8596a8c5487c2
LucasMKonopka <150478119+LucasMKonopka@users.noreply.github.com>
2024-06-05 10:58:26 -0300

    Delete ProjetoLucas/projetolucas directory

--- ProjetoLucas/projetolucas/src/main/java/com/example/projetolucas/domain/exception/BadRequestException.java
+++
@@ -1,8 +0,0 @@
-package com.example.projetolucas.domain.exception;
-
-public class BadRequestException extends RuntimeException {
-    
-    public BadRequestException(String mensagem){
-        super(mensagem);
-    }
-}
--- ProjetoLucas/projetolucas/src/main/java/com/example/projetolucas/domain/exception/ResourceNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.projetolucas.domain.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String mensagem){
-        super(mensagem);
-    }
-    
-}
